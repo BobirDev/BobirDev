3bd133c9722c87547d9679c1fec0d5dfcf52b25d
BobirDev <91252661+BobirDev@users.noreply.github.com>
2021-10-04 21:31:24 +0500

    Add files via upload

---
+++ vazifam.js
@@ -0,0 +1,38 @@
+//onst arr=[1, 2, 3,];
+//arr.push(4,5,6);
+//console.log(arr);
+
+//const arr=[1, 2, 3,];
+//arr.unshift(4,5,6);
+//console.log(arr);
+
+//const arr=[1, 2, 3, 4, 5, 6];
+//arr.pop();
+//arr.pop();
+//arr.pop();
+//console.log(arr);
+
+// const arr=[1, 2, 3, 4, 5,];
+// arr.splice(1, 2);
+// console.log(arr);
+
+// const arr = [2, 6, 1, 3, 5, 4];
+// arr.sort(function(a, b) {
+//   return a - b;
+// });
+// console.log(arr);
+
+
+// function makeArray(boshi, ohiri) {
+//       const arr = [];
+//       for (let i = boshi; i <= ohiri; i++) {
+//         arr.push(i);
+//       }
+//       console.log(arr);
+//     }
+//     makeArray(1, 10);
+
+const arr=[1, 2, 3 ];
+const arr1=[4, 5, 6 ];
+
+console.log(arr.concat(arr1));
