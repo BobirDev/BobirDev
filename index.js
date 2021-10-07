c1b7ff4ff5129e61270d10720727e64e173ff1d0
BobirDev <91252661+BobirDev@users.noreply.github.com>
2021-10-07 13:26:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// const arr = ['Apple', 'Samsung', 'Xiaomi'];
+
+// const str = arr.join(',');
+
+// console.log( str );
+
+//----------------->
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.reverse();
+
+// console.log(arr);
+
+//----------------->
+
+// const arr1=[0,1,2,3]
+// const arr2=[0,1,2,3]
+// const positive = true;
+
+// for (const i in arr2){
+// if(arr1[i] != arr2[i]){
+//  positive = false;
+// }
+// }
+
+// console.log(positive)
